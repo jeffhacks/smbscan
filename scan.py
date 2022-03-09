cf7bf93ebbc7d546da3cbee07c59a30c192e082d
Yianna Paris <yparis@seek.com.au>
2022-03-09 23:38:49 +1100

    Update import

--- scan.py
+++ scan.py
@@ -6,8 +6,7 @@ import time
 
 from slugify import slugify
 
-from smb_connect import getClient, getShares
-from smb_file_operations import getFiles
+import scan_internals
 from print import printStatus, Colors
 
 class Target:
@@ -26,6 +25,16 @@ class Target:
             # printStatus(ip, Colors.FAIL, "TARGET FAILURE: " + str(e), options)
             print(traceback.format_exc())
 
+class User:
+    def __init__(self, username="Guest", password="", domain="", lmhash="", nthash=""):
+        self.username = username
+        self.password = password
+        self.domain = domain
+        self.lmhash = lmhash
+        self.nthash = nthash
+        self.results = []
+        
+
 def scanRange(targetIPRange, user, options):
     for targetIP in ipaddress.IPv4Network(str(targetIPRange)):
         scanSingle(targetIP, user, options)
@@ -34,7 +43,7 @@ def scanRange(targetIPRange, user, options):
 def scanSingle(targetHost, user, options):
     target = Target(str(targetHost))
     # TODO This could potentially be noisier than needed. Consider only using port 445
-    smbClient = getClient(target, user, options, 445)
+    smbClient = scan_internals.getClient(target, user, options, 445)
     # if (smbClient is None):
     # 	smbClient = getClient(target, user, options, 139)
     if smbClient != None:
@@ -57,9 +66,9 @@ def scanSingle(targetHost, user, options):
                 "CONNECTED AS %1s - %2s" % (user.username, smbClient.getServerOS()),
                 options,
             )
-            getShares(smbClient, target)
+            scan_internals.getShares(smbClient, target)
             if options.crawlShares:
-                getFiles(smbClient, target, options, logFile)
+                scan_internals.getFiles(smbClient, target, options, logFile)
             user.results.append(target)
         except Exception as e:
             printStatus(target, Colors.FAIL, "GENERAL FAILURE: " + str(e), options)
