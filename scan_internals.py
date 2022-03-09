000cdd9a1868f6d63203bb63929324b36365ea3f
Yianna Paris <yparis@seek.com.au>
2022-03-09 23:36:37 +1100

    Combine files for clearer dependency needs

--- scan_internals.py
+++ scan_internals.py
@@ -3,18 +3,94 @@ import random
 import re
 import time
 
+from impacket.smbconnection import SMBConnection, SessionError
+
 from local_logging import createLogEntry
 from print import printStatus, Colors
 
+
+class Share:
+    def __init__(self, shareName):
+        self.shareName   = shareName
+        self.sharedFiles = []
+
 class SharedFile:
     def __init__(self, fileName, fullPath, isDirectory, cTime, mTime, aTime, fileSize):
-        self.fileName = fileName
-        self.fullPath = fullPath
+        self.fileName    = fileName
+        self.fullPath    = fullPath
         self.isDirectory = isDirectory
-        self.cTime = cTime
-        self.mTime = mTime
-        self.aTime = aTime
-        self.fileSize = fileSize
+        self.cTime       = cTime
+        self.mTime       = mTime
+        self.aTime       = aTime
+        self.fileSize    = fileSize
+
+def getShares(smbClient, target):
+    resp = smbClient.listShares()
+    for i in range(len(resp)):
+        shareName = resp[i]["shi1_netname"][:-1]
+        if shareName not in ["NETLOGON", "SYSVOL", "IPC$", "print$"]:
+            target.shares.append(Share(shareName))
+
+def getClient(target, user, options, port):
+    try:
+        smbClient = SMBConnection(
+            "*\\*SMBSERVER*", target.ip, timeout=options.timeout, sess_port=int(port)
+        )
+        if options.kerberos is True:
+            smbClient.kerberosLogin(
+                user.username,
+                user.password,
+                user.domain,
+                user.lmhash,
+                user.nthash,
+                options.aesKey,
+                options.dc_ip,
+            )
+        else:
+            # smbClient.login(user.username, user.password, user.domain, user.lmhash, user.nthash, ntlmFallback=False)
+            # TODO I'm not sure of any downsides to allowing ntlmFallback here...
+            smbClient.login(
+                user.username,
+                user.password,
+                user.domain,
+                user.lmhash,
+                user.nthash,
+                ntlmFallback=True,
+            )
+        # Host is live
+        return smbClient
+    except SessionError as e:
+        printStatus(
+            target,
+            Colors.WARNING,
+            "SESSION FAILURE: (%1s, %2s) %3s" % (user.username, str(port), str(e)),
+            options,
+        )
+        # Host is live
+        return None
+    except Exception as e:
+        if "timed out" in str(e):
+            None  # printStatus(target, Colors.WARNING, "CONNECTION FAILURE: " + str(e), options)
+            # Host is not live
+        elif "Connection refused" in str(e):
+            printStatus(
+                target,
+                Colors.WARNING,
+                "CONNECTION FAILURE: (%1s, %2s) %3s"
+                % (user.username, str(port), str(e)),
+                options,
+            )
+            # Host is live
+        else:
+            printStatus(
+                target,
+                Colors.FAIL,
+                "CONNECTION FAILURE: (%1s, %2s) %3s"
+                % (user.username, str(port), str(e)),
+                options,
+            )
+            # Host is live
+        return None
 
 def listFiles(target, smbClient, share, sharePath, options, logFile, currentDepth):
     try:
@@ -23,13 +99,13 @@ def listFiles(target, smbClient, share, sharePath, options, logFile, currentDept
                 continue
 
             sharedFile = SharedFile(
-                fileName=f.get_longname(),
-                fullPath=(sharePath + "\\" + f.get_longname()).strip(),
-                isDirectory=f.is_directory(),
-                cTime=time.ctime(float(f.get_ctime_epoch())),
-                mTime=time.ctime(float(f.get_mtime_epoch())),
-                aTime=time.ctime(float(f.get_atime_epoch())),
-                fileSize=f.get_filesize(),
+                fileName    = f.get_longname(),
+                fullPath    = (sharePath + "\\" + f.get_longname()).strip(),
+                isDirectory = f.is_directory(),
+                cTime       = time.ctime(float(f.get_ctime_epoch())),
+                mTime       = time.ctime(float(f.get_mtime_epoch())),
+                aTime       = time.ctime(float(f.get_atime_epoch())),
+                fileSize    = f.get_filesize(),
             )
             share.sharedFiles.append(sharedFile)
 
@@ -93,8 +169,6 @@ def listFiles(target, smbClient, share, sharePath, options, logFile, currentDept
     finally:
         if options.jitter > 0:
             time.sleep(random.randint(0, options.jitter))
-
-
 
 def getFiles(smbClient, target, options, logFile):
     for share in target.shares:
