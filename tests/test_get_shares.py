f9a10886f4a98f1b199b0d163cd0c68bd19b05fc
Yianna Paris <yparis@seek.com.au>
2022-05-14 17:05:41 +1000

    Refactor get_shares so it returns result instead of using output argument

--- tests/test_get_shares.py
+++ tests/test_get_shares.py
@@ -15,19 +15,9 @@ def test_client():
   
   return TestClient()
 
-@pytest.fixture
-def test_target():
-  class TestTarget:
-    def __init__(self):
-        self.shares      = []
-    
-  return TestTarget()
-
-
-def test_get_shares(test_client, test_target):
-  scan_internals.get_shares(test_client, test_target)
-  print(test_target.shares)
+def test_get_shares(test_client):
+  shares = scan_internals.get_shares(test_client)
   
-  assert(len(test_target.shares) == 2)
-  assert(test_target.shares[0].shareName == "Admin$")
-  assert(test_target.shares[1].shareName == "TestShare$")
+  assert(len(shares) == 2)
+  assert(shares[0].shareName == "Admin$")
+  assert(shares[1].shareName == "TestShare$")
