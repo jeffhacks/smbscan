efc33d04d539f214e9cc76daf9061ea97b9ed26c
Yianna Paris <yparis@seek.com.au>
2022-05-17 15:45:18 +1000

    Reformat test file

--- tests/test_get_shares.py
+++ tests/test_get_shares.py
@@ -2,6 +2,12 @@ import pytest
 
 import scan_internals
 
+def test_get_shares(test_client):
+  shares = scan_internals.get_shares(test_client)
+  
+  assert(len(shares) == 2)
+  assert(shares[0].shareName == "Admin$")
+  assert(shares[1].shareName == "TestShare$")
 
 @pytest.fixture
 def test_client():
@@ -14,10 +20,3 @@ def test_client():
       ]
   
   return TestClient()
-
-def test_get_shares(test_client):
-  shares = scan_internals.get_shares(test_client)
-  
-  assert(len(shares) == 2)
-  assert(shares[0].shareName == "Admin$")
-  assert(shares[1].shareName == "TestShare$")
