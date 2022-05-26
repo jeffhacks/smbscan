f0f69429e8657110595e937581f3bfd6c13e6e35
Yianna Paris <yparis@seek.com.au>
2022-05-26 21:20:00 +1000

    Refactor tests and add docstrings to mock functions

--- tests/test_scan_internals.py
+++ tests/test_scan_internals.py
@@ -1,4 +1,3 @@
-from ast import arg
 import pytest
 from unittest.mock import patch
 
@@ -15,39 +14,37 @@ def test_get_shares(test_client):
   assert(shares[0].shareName == "Admin")
   assert(shares[1].shareName == "TestShare")
 
-
 def test_get_client(test_target, test_user, test_options):
-  def mock_connection_init(self, remoteName='', remoteHost='', myName=None, sess_port=impacket.nmb.SMB_SESSION_PORT, timeout=60, 
-                          preferredDialect=None, existingConnection=None, manualNegotiate=False):
-    self._SMBConnection         = 0
-    self._dialect               = ''
-    self._nmbSession            = 0
-    self._sess_port             = sess_port
-    self._myName                = myName
-    self._remoteHost            = remoteHost
-    self._remoteName            = remoteName
-    self._timeout               = timeout
-    self._preferredDialect      = preferredDialect
-    self._existingConnection    = existingConnection
-    self._manualNegotiate       = manualNegotiate
-    self._doKerberos            = False
-    self._kdcHost               = None
-    self._useCache              = True
-    self._ntlmFallback          = True
-
-  def mock_login(self, user, password, domain = '', lmhash = '', nthash = '', ntlmFallback = True):
-    return None
-    
   with patch.object(impacket.smbconnection.SMBConnection, '__init__', mock_connection_init):
     with patch.object(impacket.smbconnection.SMBConnection, 'login', mock_login):
       port = 445
       client = scan_internals.get_client(test_target, test_user, test_options, port)
+      
       assert(client != None)
       assert(client._remoteHost == test_target.ip)
 
+def mock_connection_init(self, remoteName='', remoteHost='', myName=None, sess_port=impacket.nmb.SMB_SESSION_PORT, 
+                         timeout=60, preferredDialect=None, existingConnection=None, manualNegotiate=False):
+  """Mock SMBConnection constructor and return default values."""
+  self._SMBConnection         = 0
+  self._dialect               = ''
+  self._nmbSession            = 0
+  self._sess_port             = sess_port
+  self._myName                = myName
+  self._remoteHost            = remoteHost
+  self._remoteName            = remoteName
+  self._timeout               = timeout
+  self._preferredDialect      = preferredDialect
+  self._existingConnection    = existingConnection
+  self._manualNegotiate       = manualNegotiate
+  self._doKerberos            = False
+  self._kdcHost               = None
+  self._useCache              = True
+  self._ntlmFallback          = True
 
-class TestGetClient:
-  pass
+def mock_login(self, user, password, domain = '', lmhash = '', nthash = '', ntlmFallback = True):
+  """Mock SMBConnection login() and return None"""
+  return None
 
 @pytest.fixture
 def test_target():
@@ -71,5 +68,4 @@ def test_client():
         {"shi1_netname": "Admin\\"},
         {"shi1_netname": "TestShare\\"} 
       ]
-  
   return TestClient()
